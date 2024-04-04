daec05ab357de1c88e127928c7031d68aac8a14f
Sezayir Dagtekin <sezayir@hotmail.com>
2024-04-04 15:41:45 +0200

    mapping to domain model

--- bookstore/src/main/java/com/sezo/Book.java
+++ bookstore/src/main/java/com/sezo/Book.java
@@ -1,7 +1,9 @@
 package com.sezo;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -9,15 +11,33 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "T_BOOK")
 public class Book {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(length = 50)
     private String isbn;
+
+    @Column(length = 200)
     private String title;
+
+    @Column(length = 10000)
     private String description;
+
     private BigDecimal price;
+
+    @Column(name = "publication_date")
     private LocalDate publicationDate;
+
+    @Column(name = "number_of_pages")
     private Integer nbOfPages;
+
+    @Column(name = "image_url")
     private String imageURL;
 
 
